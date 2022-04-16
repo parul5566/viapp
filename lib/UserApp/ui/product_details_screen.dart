68887dba6f8ce13b8ff40d8af6d38ab0e6d9521f
ParulSharma <parent0077@gmail.com>
2022-04-16 10:19:01 +0530

    git add

--- lib/UserApp/ui/product_details_screen.dart
+++ lib/UserApp/ui/product_details_screen.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -158,3 +159,4 @@ class _ProductDetailsState extends State<ProductDetails> {
     );
   }
 }
+*/
